3515b454ea26201aeb73b3c76595a3be74dbab65
Caslav Bakic <caslav.bakic@synergysuite.com>
2023-10-11 12:50:39 +0200

    working on the form still

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,17 +1,51 @@
+import { useEffect, useReducer } from "react";
 import styles from "./Login.module.css";
 
 function Login({ changeLoginState }) {
-  const handleForm = (event) => {
+  const [loginData, dispatch] = useReducer(
+    (state, action) => {
+      if (action.type === "usr") state = { ...state, username: action.payload };
+      if (action.type === "pw") state = { ...state, password: action.payload };
+      if (action.type === "reset") state = { username: "", password: "" };
+      return state;
+    },
+    { username: "", password: "" }
+  );
+
+  //   useEffect();
+
+  const updateUsername = (event) => {
+    dispatch({ type: "usr", payload: event.target.value });
+  };
+
+  const updatePassword = (event) => {
+    dispatch({ type: "pw", payload: event.target.value });
+  };
+
+  //TODO: napravi handler i promijeni stil
+  const handleForm = async (event) => {
     event.preventDefault();
-    changeLoginState();
+    const { username, password } = loginData;
+    console.log(username, password);
+    // changeLoginState();
   };
   return (
     <div className={styles.login}>
-      <form className={styles.login__form} onSubmit={(e) => handleForm(e)}>
+      <form className={styles.login__form} onSubmit={handleForm}>
         <p>Username:</p>
-        <input id="usernameInput" type="text" placeholder="Unesi username.." />
+        <input
+          id="usernameInput"
+          type="text"
+          placeholder="Unesi username.."
+          onChange={updateUsername}
+        />
         <p>Password:</p>
-        <input id="passwordInput" type="password" placeholder="Unesi sifru.." />
+        <input
+          id="passwordInput"
+          type="password"
+          placeholder="Unesi sifru.."
+          onChange={updatePassword}
+        />
         <button>Login</button>
       </form>
     </div>
